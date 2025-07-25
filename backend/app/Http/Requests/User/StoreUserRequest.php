0996e7f592ca023a28c08c565ca5e11395cbc9c6
pragith98 <lakshanxp@hotmail.com>
2025-07-25 16:14:29 +0530

    feat #5: Create user store,update and update-password requests

---
+++ backend/app/Http/Requests/User/StoreUserRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required','max:255'],
+            'email' => [
+                'required',
+                'email',
+                'max:255',
+                Rule::unique('users', 'email')
+            ],
+            'telephone' => [
+                'required',
+                'max:10',
+                Rule::unique('users', 'telephone')
+            ],
+            'nic' => [
+                'required',
+                'max:12',
+                Rule::unique('users', 'nic')
+            ],
+            'address' => ['required','max:255'],
+        ];
+    }
+}
