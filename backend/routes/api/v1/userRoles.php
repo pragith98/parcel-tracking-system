099b28b318e863297ea36013b0c1685ea5e84012
pragith98 <lakshanxp@hotmail.com>
2025-07-25 21:41:53 +0530

    feat #5: Create authetication middleware

--- backend/routes/api/v1/userRoles.php
+++ backend/routes/api/v1/userRoles.php
@@ -4,11 +4,13 @@ use App\Http\Controllers\UserRoleController;
 use App\Http\Middleware\AuthenticateWithSanctumCookie;
 use Illuminate\Support\Facades\Route;
 
-// Route::middleware(AuthenticateWithSanctumCookie::class)
-Route::prefix('user-roles')->group(function () {
-    Route::get('/', [UserRoleController::class, 'index']);
-    Route::get('/{id}', [UserRoleController::class, 'show']);
-    Route::post('/', [UserRoleController::class, 'store']);
-    Route::put('/{id}', [UserRoleController::class, 'update']);
-    Route::delete('/{id}', [UserRoleController::class, 'destroy']);
-});
+Route::middleware(AuthenticateWithSanctumCookie::class)
+    ->prefix('user-roles')
+    ->group(function () {
+        Route::get('/', [UserRoleController::class, 'index']);
+        Route::get('/{id}', [UserRoleController::class, 'show']);
+        Route::post('/', [UserRoleController::class, 'store']);
+        Route::put('/{id}', [UserRoleController::class, 'update']);
+        Route::delete('/{id}', [UserRoleController::class, 'destroy']);
+    }
+);
