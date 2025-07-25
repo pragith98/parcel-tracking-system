da9c653ff0e701c670272819186c81b159896fcb
pragith98 <lakshanxp@hotmail.com>
2025-07-25 22:40:12 +0530

    feat #7: Create parcel store and update requests

--- backend/database/migrations/2025_07_25_163955_parcel.php
+++ backend/database/migrations/2025_07_25_163955_parcel.php
@@ -13,18 +13,18 @@ return new class extends Migration
     {
         Schema::create('parcel', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('remarks', 255);
+            $table->string('remarks', 255)->nullable();
             $table->string('receiverName', 255);
-            $table->string('receiverTelephone', 255);
+            $table->string('receiverTelephone', 10);
             $table->string('receiverAddress', 255);
             $table->string('receiverEmail', 255);
             $table->string('senderName', 255);
-            $table->string('senderTelephone', 255);
+            $table->string('senderTelephone', 10);
             $table->string('senderAddress', 255);
             $table->string('senderEmail', 255);
-            $table->date('estimatedDelivery_date');
-            $table->date('pickedUpAt');
-            $table->date('deliveredAt');
+            $table->date('estimatedDeliveryDate')->nullable();
+            $table->date('pickedUpAt')->nullable();
+            $table->date('deliveredAt')->nullable();
             $table->string('code', 255);
             $table->timestamps();
         });
