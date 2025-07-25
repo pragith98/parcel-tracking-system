43c127d781e5eb4434f213894f3bf050333582d3
pragith98 <lakshanxp@hotmail.com>
2025-07-25 16:26:15 +0530

    feat #5: Create user resource and collection

---
+++ backend/app/Http/Resources/User/UserResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources\User;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'telephone' => $this->telephone,
+            'nic' => $this->nic,
+            'address' => $this->address,
+        ];
+    }
+
+    public function with(Request $request)
+    {
+        return [
+            'success' => true
+        ];
+    }
+}
