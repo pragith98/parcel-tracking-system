968a2c65a261099c898a860136821d940ff1737a
pragith98 <lakshanxp@hotmail.com>
2025-07-26 14:46:05 +0530

    feat #9: Create tracking-history resource

---
+++ backend/app/Http/Resources/TrackingHistory/TrackingHistoryResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\TrackingHistory;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TrackingHistoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'parcelId' => $this->parcelId,
+            'note' => $this->note,
+            'status' => $this->status,
+            'updatedBy' => $this->updatedBy
+        ];
+    }
+
+    public function with(Request $request)
+    {
+        return [
+            'success' => true
+        ];
+    }
+}
