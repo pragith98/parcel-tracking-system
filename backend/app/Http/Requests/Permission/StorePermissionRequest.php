4bcf653e8c6a9357412fbe37aebd2eaa8e6fabf0
pragith98 <lakshanxp@hotmail.com>
2025-07-26 22:05:23 +0530

    feat #11: Create permission store request

---
+++ backend/app/Http/Requests/Permission/StorePermissionRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\Permission;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+class StorePermissionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required','max:255'],
+            'userRoleId' => [
+                'required',
+                'max:255',
+                Rule::exists('user_roles', 'id')
+            ],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        // Get validation errors as a flat array
+        $errors = $validator->errors()->all();
+
+        throw new HttpResponseException(response()->json(['errors' => $errors], 422));
+    }
+}
