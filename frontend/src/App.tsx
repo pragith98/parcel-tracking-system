2d30ec2d52bffefa7387606447f73d43c2be743d
pragith98 <lakshanxp@hotmail.com>
2025-07-27 20:43:37 +0530

    feat #15: Create footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import './index.css'
+import Footer from "./components/Footer";
+import "./index.css";
 
 function App() {
   return (
     <>
-      <p className='text-red-600'>hello</p>
+      <p className="text-red-600">hello</p>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
