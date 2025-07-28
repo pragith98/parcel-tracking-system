18b4a8c0bc979ce8018af6f7a168faa92cc6e234
pragith98 <lakshanxp@hotmail.com>
2025-07-28 15:31:06 +0530

    feat #15: Create user-roles page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainMenu from "./components/MainMenu";
 import "./index.css";
-import AllUsersPage from "./pages/AllUsersPage";
+import UserRolesPage from "./pages/UserRolesPage";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Header />
 
         <div>
-          <AllUsersPage />
+          <UserRolesPage />
         </div>
 
         <Footer />
---
+++ frontend/src/pages/UserRolesPage.tsx
@@ -0,0 +1,54 @@
+import TablePagination from "../components/TablePagination";
+import { TableStyles } from "../styles";
+
+function UserRolesPage() {
+  const getUserRoles = () => {
+    let count = 0;
+    return userRoles.map((role) => {
+      count++;
+      return { ...role, index: count };
+    });
+  };
+
+  return (
+    <div className="p-2">
+      <table className={TableStyles.table}>
+        <thead className={TableStyles.header}>
+          <tr>
+            <th scope="col" className={TableStyles.headerRow}>
+              <span>#</span>
+            </th>
+
+            <th scope="col" className={TableStyles.headerRow}>
+              <span>Name</span>
+            </th>
+          </tr>
+        </thead>
+
+        <tbody className={TableStyles.body}>
+          {getUserRoles().map((item) => (
+            <tr className={TableStyles.row} key={item.index}>
+              <td className={TableStyles.data}>
+                <span className="text-xs" >{item.index}</span>
+              </td>
+
+              <td className={TableStyles.data}>
+                <span>{item.name}</span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <TablePagination />
+    </div>
+  );
+}
+
+const userRoles = [
+  { id: "1", name: "ADMIN" },
+  { id: "2", name: "OWNER" },
+  { id: "3", name: "WORKER" },
+];
+
+export default UserRolesPage;
