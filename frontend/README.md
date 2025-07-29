aaecb9d8c7f3c6e665ea917c3f39d92370ad4250
pragith98 <lakshanxp@hotmail.com>
2025-07-29 14:59:23 +0530

    feat #17: Setup routes with main menu

--- frontend/README.md
+++ frontend/README.md
@@ -3,3 +3,4 @@
 ## Installed Dependencies
 1. npm install tailwindcss @tailwindcss/vite
 2. npm i react-icons
+3. npm install react-router-dom
---
+++ frontend/src/constants/routes.ts
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  HOME: '/',
+  PARCELS: '/parcels',
+  USERS: '/users',
+  USER_ROLES: '/user-roles',
+  PERMISSIONS: '/permissions',
+  COMPANY: '/company',
+  NOT_FOUND: '*'
+}
